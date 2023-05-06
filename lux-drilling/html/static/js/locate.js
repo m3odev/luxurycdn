79c30054ee319370dd9c25a1a7d7c25341403179
luxurycity <116413978+luxurycity@users.noreply.github.com>
2023-05-06 08:27:59 +0700

    Add files via upload

---
+++ lux-drilling/html/static/js/locate.js
@@ -0,0 +1,12 @@
+var Locate = {
+    "qb":"Tình Trạng",
+    "Cc":"Công Nhân",
+    "Gf":"Dàn Khoan",
+    "GW":"Dầu",
+    "PI":"Khoan Đường Đến Các",
+    "x9":"Tháp Dầu",
+    "SG":"Giữ Chuột Trái",
+    "i_":"Để Khoan",
+    "dF":"Dầu Thô",
+    "by":"Dầu Loãng"
+}
